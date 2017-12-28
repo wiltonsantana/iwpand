284e5cf1662af8bfd74b195cfbb189a15efbb89d
Claudio Takahasi <ckt@cesar.org.br>
2017-12-28 09:53:25 -0300

    Add ELL dependency
    This patch changes the Makefile adding Embedded Linux Library dependency.
    Symbolic links are created pointing to ELL source. ELL source code should
    be placed at the same top level directory as the Wireless PAN source.

--- src/main.c
+++ src/main.c
@@ -24,7 +24,20 @@
 #include <config.h>
 #endif
 
+#include <ell/ell.h>
+
 int main(int argc, char *argv[])
 {
+	if (!l_main_init())
+		return EXIT_FAILURE;
+
+	l_log_set_stderr();
+	l_debug_enable("*");
+	l_info("Wireless PAN daemon version %s", VERSION);
+
+	l_main_run();
+
+	l_main_exit();
+
 	return 0;
 }
