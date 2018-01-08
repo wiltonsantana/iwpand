8da81a8fc5d07d7b5d78cc5b194971dc94b730e3
Claudio Takahasi <ckt@cesar.org.br>
2018-01-08 14:53:53 -0300

    Add tracking virtual link creation
    This patch adds netink callbacks to track when a new 6lowpan virtual
    interface is created.

--- src/lowpan.c
+++ src/lowpan.c
@@ -26,13 +26,60 @@
 
 #include <stdbool.h>
 
+#include <linux/if_arp.h>
+#include <linux/rtnetlink.h>
+
+#include <ell/ell.h>
+
 #include "lowpan.h"
+
+static struct l_netlink *rtnl = NULL;
+
+static void rtnl_link_notify(uint16_t type, const void *data, uint32_t len,
+							void *user_data)
+{
+	/*
+	 * ip link add link wpan0 name lowpan0 type lowpan
+	 * Callback called when virtual link is created or deleted.
+	 */
+	const struct ifinfomsg *ifi = data;
+
+	if (ifi->ifi_type != ARPHRD_6LOWPAN)
+		return;
+
+	switch (type) {
+	case RTM_NEWLINK:
+		l_info("RTNL_NEWLINK");
+		break;
+	case RTM_DELLINK:
+		l_info("RTM_DELLINK");
+		break;
+	}
+}
 
 bool lowpan_init(void)
 {
+	l_info("6LoWPAN init");
+
+	rtnl = l_netlink_new(NETLINK_ROUTE);
+	if (!rtnl) {
+		l_error("Failed to open netlink route socket");
+		return false;
+	}
+
+	if (!l_netlink_register(rtnl, RTNLGRP_LINK,
+				rtnl_link_notify, NULL, NULL)) {
+		l_error("Failed to register RTNL link notifications");
+		l_netlink_destroy(rtnl);
+		return false;
+	}
+
 	return true;
 }
 
 void lowpan_exit(void)
 {
+	l_info("6LoWPAN exit");
+
+	l_netlink_destroy(rtnl);
 }
